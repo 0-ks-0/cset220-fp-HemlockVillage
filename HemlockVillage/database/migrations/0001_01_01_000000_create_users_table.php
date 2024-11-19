49003b7152a9b80f1163a739e56ea879a9766f6e
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-11-19 15:37:32 -0500

    Create User model and migration

--- HemlockVillage/database/migrations/0001_01_01_000000_create_users_table.php
+++ HemlockVillage/database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,13 +12,24 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->id()->unsigned();
+            $table->string('first_name', 50);
+            $table->string("last_name", 50);
+            $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->date("date_of_birth");
+            $table->string("phone_number", 20);
             $table->string('password');
             $table->rememberToken();
+            $table->unsignedBigInteger("role_id")->unsigned()->nullable();
+            $table->boolean("approved")->default(0);
             $table->timestamps();
+
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onUpdate("cascade")
+                ->onDelete("set null");
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
