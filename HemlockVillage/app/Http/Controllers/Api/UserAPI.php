76f8626e6f70eb241ad74b0857877049f3aeea6b
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-11-25 20:45:47 -0500

    Add POST for sign up
    Validate input and create accounts

--- HemlockVillage/app/Http/Controllers/Api/UserAPI.php
+++ HemlockVillage/app/Http/Controllers/Api/UserAPI.php
@@ -39,7 +39,82 @@ class UserAPI extends Controller
      */
     public function store(Request $request)
     {
+         // Validate user info
+         $validatedUser = Validator::make($request->all(), [
+            "role" => [ "required", "exists:roles,id" ],
+            "first_name" => [ "required", "max:50" ],
+            "last_name" => [ "required", "max:50" ],
+            "email" => [ "bail", "required", "email", "unique:users", "max:100" ],
+            "date_of_birth" => [ "required", "date", "before:". date("Y-m-d"), "date_format:Y-m-d" ],
+            "phone_number" => [ "required", "max:20" ],
+            "password" => [ "bail", "required", "confirmed" ],
+            "family_code" => [ "required_if:role," . Role::getId("Patient"), "in:" . session("familyCode"), "unique:patients", "size:16"],
+            "econtact_name" => [ "required_if:role," . Role::getId("Patient"), "max:128" ],
+            "econtact_phone" => [ "required_if:role," . Role::getId("Patient"), "max:20" ],
+            "econtact_relation" => [ "required_if:role," . Role::getId("Patient"), "max:50" ],
+        ]);
 
+        // Fails validation for user
+        if ($validatedUser->fails())
+            return redirect()->back()->withErrors([ "Invalid input(s). Please try again."]);
+
+        // Hash the password
+        $request->merge([
+            "password" => Hash::make($request->get("password"))
+        ]);
+
+        User::create([
+            "first_name" => $request->get("first_name"),
+            "last_name" => $request->get("last_name"),
+            "email" => $request->get("email"),
+            "date_of_birth" => $request->get("date_of_birth"),
+            "phone_number" => $request->get("phone_number"),
+            "password" => $request->get("password"),
+            "role_id" => $request->get("role"),
+            "approved" => 0
+        ]);
+
+        $userId = User::getId($request->get("email"));
+
+        switch ($request->role)
+        {
+            case Role::getId("Admin"):
+            case Role::getId("Supervisor"):
+            case Role::getId("Doctor"):
+            case Role::getId("Caregiver"):
+                Employee::create([
+                    "user_id" => $userId
+                ]);
+
+                break;
+
+            case Role::getId("Patient"):
+                Patient::create([
+                    "id" => ModelHelper::getRandomString(),
+                    "user_id" => $userId,
+                    "family_code" => $request->get("family_code"),
+                    "econtact_name" => $request->get("econtact_name"),
+                    "econtact_phone" => $request->get("econtact_phone"),
+                    "econtact_relation" => $request->get("econtact_relation")
+                ]);
+
+                break;
+
+            case Role::getId("Family"):
+                Employee::create([
+                    "user_id" => $userId
+                ]);
+
+                break;
+
+            default:
+        }
+
+        session()->forget("familyCode");
+
+        session()->flash("message", "Your account has been created successfully. Please wait for approval to login.");
+
+        return redirect()->route("login");
     }
 
     /**
