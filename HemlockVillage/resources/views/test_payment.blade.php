ed91a740b560f6d57809f0a5fe12569efbc8f4f8
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-11-27 22:49:36 -0500

    Create test_payment.blade.php
    Test using JavaScript to change the url once the patient ID is inputted

---
+++ HemlockVillage/resources/views/test_payment.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+		<title>payment testing</title>
+
+		<script src="{{ asset("./js/navigator.js") }}"></script>
+	</head>
+	<body>
+		<form>
+			<input type="text" name="patient_id" placeholder="patient id" id="input_patient_id"
+				@isset($patientId) value="{{ $patientId }}" @endisset
+			>
+		</form>
+
+		@isset($bill) <p>{{ $bill }}</p> @endisset
+		@isset($patiendId) <input type="number" placeholder="{{ $bill }}" max="{{ $bill }}"> @endisset
+
+		<script>
+			window.onload = () =>
+			{
+				patientIdInput = document.querySelector("#input_patient_id")
+				if (!patientIdInput) return
+
+				patientIdInput.oninput = (event) =>
+				{
+					event.preventDefault()
+
+					if (patientIdInput.value.length < 16) return // Don't do anything until the fixed length of patient id is reached
+
+					setTop(`/test/payment/${patientIdInput.value}`) // Change the url
+				}
+			}
+		</script>
+	</body>
+</html>
