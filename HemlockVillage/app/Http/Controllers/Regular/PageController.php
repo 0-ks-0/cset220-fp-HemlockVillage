eac95abc8f44951d55135c80dba4936bd5ec17ab
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-11-30 18:21:15 -0500

    Set up template view display control for home page

--- HemlockVillage/app/Http/Controllers/Regular/PageController.php
+++ HemlockVillage/app/Http/Controllers/Regular/PageController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Regular;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 
+use Illuminate\Support\Facades\Auth;
+
+use App\Http\Controllers\Controller;
 use App\Http\Controllers\Api\UserAPI;
+
+use App\Helpers\ControllerHelper;
 
 class PageController extends Controller
 {
@@ -23,6 +27,35 @@ class PageController extends Controller
 
     public static function home()
     {
-        return view("home");
+        $userId = Auth::user()->id;
+        $accessLevel = ControllerHelper::getUserAccessLevel($userId);
+
+        // Return view depending on access level of user
+        switch ($accessLevel)
+        {
+            case 1: // Admin
+                return redirect("/users");
+
+            case 2: // Supervisor
+                return redirect("/users");
+
+            case 3: // Doctor
+                return view("doctorshome");
+
+            case 4: // Caregiver
+                return view("caregivershome");
+
+            case 5: // Patient
+                return view("patientshome");
+
+            case 6: // Family
+                return view("familyhome");
+
+            case null:
+                return response()->json(['error' => 'Could not find user id or access level'], 404);
+
+            default:
+                return "No home page for your access level";
+        }
     }
 }
