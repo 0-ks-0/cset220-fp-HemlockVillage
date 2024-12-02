c09c4e66733fbc8d19ee1d0de325c67357ab37ca
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-12-02 10:44:12 -0500

    Add function to get prescription status, appointment info, and doctor info of by patient and date

--- HemlockVillage/app/Helpers/ControllerHelper.php
+++ HemlockVillage/app/Helpers/ControllerHelper.php
@@ -4,6 +4,8 @@ namespace App\Helpers;
 
 use Illuminate\Support\Facades\DB;
 
+use App\Models\PrescriptionStatus;
+
 class ControllerHelper
 {
 	/**
@@ -21,4 +23,17 @@ class ControllerHelper
 			})->value("access_level");
 	}
 
+	/**
+	 * Get the prescription status, appointment info for prescriptions, and the doctor info
+	 */
+	public static function getPatientPrescriptionStatusAppointmentByDate($patientId, $date)
+	{
+		return PrescriptionStatus::with(["appointment.doctor.user"])
+        ->where("prescription_date", $date)
+        ->whereHas("appointment", function ($query) use ($patientId)
+        {
+            $query->where("patient_id", "=", $patientId);
+        })->first();
+
+	}
 }
