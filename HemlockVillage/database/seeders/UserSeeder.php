641f040ef77b5dc7305fe029ed06f3d1c2eb03d9
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-12-06 13:25:08 -0500

    Combine some seeders for users and employees

--- HemlockVillage/database/seeders/UserSeeder.php
+++ HemlockVillage/database/seeders/UserSeeder.php
@@ -27,6 +27,13 @@ class UserSeeder extends Seeder
         self::insertData("Dr. Isabella", "King", "isabella.king@example.com", "1985-12-03", "555-623-4517", "doctor123", 3, 1);
         self::insertData("Sophia", "Morris", "sophia.morris@example.com", "1990-09-14", "555-302-8569", "caregiver123", 4, 1);
 
+        self::insertData("admin", "first", "a@example.com", "2000-02-02", "123-456-7890", "admin", 1, 1);
+        self::insertData("supervisor", "first", "s@example.com", "1985-09-23", "321-654-9870", "supervisor", 2, 1);
+        self::insertData("doctor", "first", "d@example.com", "1990-03-15", "456-789-1230", "doctor", 3, 1);
+        self::insertData("caregiver", "first", "c@example.com", "1992-07-10", "987-654-3210", "caregiver", 4, 1);
+        self::insertData("patient", "first", "p@example.com", "1950-02-25", "234-567-8901", "patient", 5, 1);
+        self::insertData("family", "first", "f@example.com", "1995-11-17", "345-678-9012", "family", 6, 1);
+
         self::insertData("caregiver", "two", "c2@example.com", "1992-07-10", "345-678-9013", "caregiver", 4, 1);
         self::insertData("caregiver", "three", "c3@example.com", "1995-11-17", "345-678-9014", "caregiver", 4, 1);
         self::insertData("caregiver", "four", "c4@example.com", "1995-11-17", "345-678-9015", "caregiver", 4, 1);
