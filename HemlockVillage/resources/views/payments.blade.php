f2899f8c8e3c1cf5156f7a16b0a07e6e5abe9987
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-12-07 13:18:50 -0500

    Put input-related things into a form
    So much easier than needing to use fetch API

--- HemlockVillage/resources/views/payments.blade.php
+++ HemlockVillage/resources/views/payments.blade.php
@@ -11,24 +11,26 @@
         <div class="container">
             <h1>Payments</h1>
 
-            <div class="form-group">
-                <label for="patient-id">Patient ID:</label>
-                <input type="text" id="patient-id" >
-            </div>
+            <form action="">
+                <div class="form-group">
+                    <label for="patient-id">Patient ID:</label>
+                    <input type="text" id="patient-id" >
+                </div>
 
-            <div class="form-group">
-                <label for="total-due">Total Due:</label>
-                <input type="text" id="total-due" value="$150.00" readonly>
-            </div>
+                <div class="form-group">
+                    <label for="total-due">Total Due:</label>
+                    <input type="text" id="total-due" value="$150.00" readonly>
+                </div>
 
-            <div class="form-group">
-                <label for="new-payment">New Payment:</label>
-                <input type="text" id="new-payment" >
-            </div>
+                <div class="form-group">
+                    <label for="new-payment">New Payment:</label>
+                    <input type="text" id="new-payment" >
+                </div>
 
-            <div class="form-group">
-                <button type="button">Update Payment</button>
-            </div>
+                <div class="form-group">
+                    <button type="button">Update Payment</button>
+                </div>
+            </form>
         </div>
 
         @include('navbar')
