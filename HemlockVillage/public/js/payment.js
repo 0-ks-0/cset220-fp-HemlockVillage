4141b8bc70e5aad1d725a7ab7fbfd10ac7ed43b4
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-12-08 08:11:42 -0500

    Add onclick for buttons for submission
    Submit form only if the user clicks Confirm in the modal

--- HemlockVillage/public/js/payment.js
+++ HemlockVillage/public/js/payment.js
@@ -7,11 +7,63 @@ function checkIdInput(event)
 	setTop(`/payment/${patientIdInput.value}`) // Change the url
 }
 
-
 window.onload = () =>
 {
 	patientIdInput = document.querySelector("#patient-id")
 	if (!patientIdInput) console.error("none")
 
 	patientIdInput.oninput = () => checkIdInput(event)
+
+
+	/**
+	 *
+	 * Modal
+	 *
+	 */
+	const modal = document.querySelector(".modal")
+	const payButton = document.querySelector("#pay_btn")
+	const confirmButton = document.querySelector("#confirm_btn")
+	const cancelButton = document.querySelector("#cancel_btn")
+	const closeModal = document.querySelector("#close_modal")
+	const form = document.querySelector("#payment_form")
+
+	// TODO validate that the elements exist
+
+	/**
+	 * Display
+	 */
+	payButton.onclick = () =>
+	{
+		modal.classList.add("show")
+	}
+
+	cancelButton.onclick = () =>
+	{
+		event.preventDefault()
+		modal.classList.remove("show")
+	}
+
+	closeModal.onclick = () =>
+	{
+		modal.classList.remove("show")
+	}
+
+	// Clicking outside of modal
+	window.onclick = (event) =>
+	{
+		if (event.target == modal)
+			modal.classList.remove("show")
+	}
+
+	/**
+	 * Form submission
+	 */
+	confirmButton.onclick = () =>
+	{
+		console.log("coonfirm cancel")
+
+		modal.classList.remove("show")
+
+		form.submit()
+	}
 }
