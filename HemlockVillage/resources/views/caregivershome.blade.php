ee86b90e5d372d24c89f278e6ee608f012d98afe
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-12-09 18:10:23 -0500

    Add message section as "error" and control display of patients

--- HemlockVillage/resources/views/caregivershome.blade.php
+++ HemlockVillage/resources/views/caregivershome.blade.php
@@ -28,45 +28,68 @@
                 <input type="text" id="caregiver-id" value="{{ $caregiverName ?? ''}}" readonly>
             </div>
 
-            <h2>List of Patients Today</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Meds</th>
-                        <th>Meals</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>John Doe</td>
-                        <td>Perc 30</td>
-                        <td>
-                            <label><input type="checkbox" checked> Breakfast</label><br>
-                            <label><input type="checkbox"> Lunch</label><br>
-                            <label><input type="checkbox"> Dinner</label>
-                        </td>
-                        <td>
-                            <button type="button" class="btn-success">Update Task Completion</button>
-                            <button type="button" class="btn-primary">Patient Details</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Jane Smith</td>
-                        <td>Perc 40</td>
-                        <td>
-                            <label><input type="checkbox" checked> Breakfast</label><br>
-                            <label><input type="checkbox" checked> Lunch</label><br>
-                            <label><input type="checkbox"> Dinner</label>
-                        </td>
-                        <td>
-                            <button type="button" class="btn-success">Update Task Completion</button>
-                            <button type="button" class="btn-primary">Patient Details</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            {{-- No roster created or not on roster --}}
+            @if(isset($message))
+                <div>{{ $message }}</div>
+
+            {{-- Display patients --}}
+            @else
+                <h2>List of Patients Today</h2>
+
+                @isset($data)
+                {{-- No patients --}}
+                    @if(empty($data))
+                        No patients
+
+                    {{-- Display patients --}}
+                    @else
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Meds</th>
+                                    <th>Meals</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                @foreach($data as $d)
+                                    <form action="">
+                                        <tr>
+                                            <td>John Doe</td>
+                                            <td>Perc 30</td>
+                                            <td>
+                                                <label><input type="checkbox" checked> Breakfast</label><br>
+                                                <label><input type="checkbox"> Lunch</label><br>
+                                                <label><input type="checkbox"> Dinner</label>
+                                            </td>
+                                            <td>
+                                                <button type="button" class="btn-success">Update Task Completion</button>
+                                                <button type="button" class="btn-primary">Patient Details</button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Jane Smith</td>
+                                            <td>Perc 40</td>
+                                            <td>
+                                                <label><input type="checkbox" checked> Breakfast</label><br>
+                                                <label><input type="checkbox" checked> Lunch</label><br>
+                                                <label><input type="checkbox"> Dinner</label>
+                                            </td>
+                                            <td>
+                                                <button type="button" class="btn-success">Update Task Completion</button>
+                                                <button type="button" class="btn-primary">Patient Details</button>
+                                            </td>
+                                        </tr>
+                                    </form>
+                                @endforeach
+                            </tbody>
+                        @endif
+                    @endisset
+                </table>
+            @endif
+
 
             <div class="form-group">
                 <button type="button" class="btn-secondary">Dashboard</button>
