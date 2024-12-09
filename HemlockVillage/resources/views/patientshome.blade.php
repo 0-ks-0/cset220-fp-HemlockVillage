c84e6b9aaf5fa40a103bf91007469e1688bee416
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-12-09 15:03:23 -0500

    Use asset for stylesheet href. Change the stylesheet
    Wrong stylesheet apparently

--- HemlockVillage/resources/views/patientshome.blade.php
+++ HemlockVillage/resources/views/patientshome.blade.php
@@ -1,8 +1,8 @@
 <html>
     <head>
         <title>Patients Home</title>
-        <link rel="stylesheet" href="css/app.css">
 
+        <link rel="stylesheet" href="{{ asset('./css/patientshome.css') }}">
     </head>
 
 
