8df0c3ba3f483c95ba25e07804ddd4e0f30a7b15
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-12-09 13:12:06 -0500

    Use asset for stylesheet href

--- HemlockVillage/resources/views/doctorshome.blade.php
+++ HemlockVillage/resources/views/doctorshome.blade.php
@@ -4,7 +4,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Doctors Home</title>
-        <link rel="stylesheet" href="css/doctorshome.css">
+        <link rel="stylesheet" href="{{ asset('css/doctorshome.css') }}">
 
         <script src="{{ asset('./js/navigator.js') }}"></script>
     </head>
