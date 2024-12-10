bc6770092b5725a412c97ae9a284ad0e75871b40
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-12-10 11:07:03 -0500

    Load actual data into page

--- HemlockVillage/resources/views/adminreport.blade.php
+++ HemlockVillage/resources/views/adminreport.blade.php
@@ -25,9 +25,11 @@
             </div>
 
             <h2>Missed Patient Activity</h2>
+
             <table>
                 <thead>
                     <tr>
+                        <th>Patient's Name</th>
                         <th>Doctor's Name</th>
                         <th>Doctor's Appointment</th>
                         <th>Caregiver's Name</th>
@@ -39,30 +41,33 @@
                         <th>Dinner</th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr>
-                        <td>Dr. Helock</td>
-                        <td>2024-11-18</td>
-                        <td>Michael Green</td>
-                        <td><input type="checkbox" name="morning-meds" id="morning_meds"></td>
-                        <td><input type="checkbox" name="afternoon-meds" id="afternoon_meds"></td>
-                        <td><input type="checkbox" name="night-meds" id="night_meds"></td>
-                        <td><input type="checkbox" name="breakfast" id="breakfast"></td>
-                        <td><input type="checkbox" name="lunch" id="lunch"></td>
-                        <td><input type="checkbox" name="dinner" id="dinner"></td>
-                    </tr>
-                    <tr>
-                        <td>Dr. Cooper</td>
-                        <td>2024-11-17</td>
-                        <td>Emily White</td>
-                        <td><input type="checkbox" name="morning-meds" id="morning_meds"></td>
-                        <td><input type="checkbox" name="afternoon-meds" id="afternoon_meds"></td>
-                        <td><input type="checkbox" name="night-meds" id="night_meds"></td>
-                        <td><input type="checkbox" name="breakfast" id="breakfast"></td>
-                        <td><input type="checkbox" name="lunch" id="lunch"></td>
-                        <td><input type="checkbox" name="dinner" id="dinner"></td>
-                    </tr>
-                </tbody>
+
+                @if($data->isEmpty())
+                    No patients has missed any activity for today
+
+                @else
+                    <tbody>
+                        @foreach($data as $d)
+                            <tr>
+                                {{-- Patient, doctor, caregiver --}}
+                                <td>{{ $d["patient_name"] ?? "" }}</td>
+                                <td>{{ $d["doctor_name"] ?? "None" }}</td>
+                                <td>{{ $d["appointment_status"] ?? "None" }}</td>
+                                <td>{{ $d["caregiver_name"] ?? "None" }}</td>
+
+                                {{-- Meds --}}
+                                <td>{{ $d["prescriptions_status"]["morning"] ?? "None" }}</td>
+                                <td>{{ $d["prescriptions_status"]["afternoon"] ?? "None" }}</td>
+                                <td>{{ $d["prescriptions_status"]["night"] ?? "None" }}</td>
+
+                                {{-- Meals --}}
+                                <td>{{ $d["meal_status"]["breakfast"] ?? "None" }}</td>
+                                <td>{{ $d["meal_status"]["lunch"] ?? "None" }}</td>
+                                <td>{{ $d["meal_status"]["dinner"] ?? "None" }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                @endif
             </table>
         </div>
 
