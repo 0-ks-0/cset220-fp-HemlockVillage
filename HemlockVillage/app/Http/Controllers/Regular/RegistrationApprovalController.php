11f2d9232f5b520c83e99af902406e8865030284
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-12-10 23:41:21 -0500

    Handle CUD operations for  patient and employee
    - Update or delete patient
    - Create or delete employee

--- HemlockVillage/app/Http/Controllers/Regular/RegistrationApprovalController.php
+++ HemlockVillage/app/Http/Controllers/Regular/RegistrationApprovalController.php
@@ -2,9 +2,15 @@
 namespace App\Http\Controllers\Regular;
 
 use App\Http\Controllers\Controller;
+
 use App\Models\User;
+use App\Models\Patient;
+use App\Models\Employee;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
+use Carbon\Carbon;
 
 class RegistrationApprovalController extends Controller
 {
@@ -26,6 +32,27 @@ class RegistrationApprovalController extends Controller
             return redirect()->route('registrationapproval.index')->with('error', 'User not found');
         }
 
+        // Create employee if employee role
+        if (in_array($user->role_id, [ 1, 2, 3, 4 ]))
+            Employee::create([ "user_id" => $user->id ]);
+
+        /**
+         *  Patient
+         */
+        $patient = Patient::where("user_id", $user->id)->first();
+
+        if ($patient)
+        {
+            $patient->update([
+                "admission_date" => Carbon::today()->toDateString(),
+                "daily_updated_date" => Carbon::today()->toDateString(),
+                "prescription_updated_date" => Carbon::today()->toDateString()
+            ]);
+        }
+
+        /**
+         * Change approved status
+         */
         $user->approved = true; // Set the user as approved
         $user->save();
 
@@ -39,6 +66,13 @@ class RegistrationApprovalController extends Controller
         if (!$user) {
             return redirect()->route('registrationapproval.index')->with('error', 'User not found');
         }
+
+         /**
+         * Delete from patients table if needed
+         */
+        $patient = Patient::where("user_id", $user->id)->first();
+
+        if ($patient) $patient->delete(); // Patient found, delete
 
         $user->delete(); // Remove the user from the database
 
