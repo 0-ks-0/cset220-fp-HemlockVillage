5599216a2875d2a4054e487f4ae62032f517c088
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-12-12 12:24:14 -0500

    Add more test data for the video

--- HemlockVillage/database/seeders/AppointmentSeeder.php
+++ HemlockVillage/database/seeders/AppointmentSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Seeder;
 
 use App\Models\Appointment;
 use App\Models\Patient;
+use App\Models\Employee;
 
 use DateTime;
 
@@ -18,27 +19,56 @@ class AppointmentSeeder extends Seeder
     public function run(): void
     {
         // Don't touch
-        self::insertData(Patient::getId(5), "2024-11-01", "2024-11-02", 3, "Completed", "low on iron", "take 6mg iron supplement", "take 6mg iron supplement", null);
-        self::insertData(Patient::getId(5), "2024-11-02", "2024-12-03", 3, "Completed", "slightly low on iron", "take 6mg iron supplement", null, null);
-        self::insertData(Patient::getId(5), date("Y-m-d"), "2025-01-01", 3, "Pending");
+        self::insertData(Patient::getId(5), "2024-11-01", "2024-11-02", 3, "Completed", "low on iron", "take 6mg iron supplement", "take 6mg iron supplement", null); // 1
+        self::insertData(Patient::getId(5), "2024-11-02", "2024-12-03", 3, "Completed", "slightly low on iron", "take 6mg iron supplement", null, null); // 2
+        self::insertData(Patient::getId(5), "2024-11-04", "2025-01-01", 3, "Pending"); // 3
 
-        self::insertData(Patient::getId(5), '2024-11-05', '2024-11-06', 3, 'Completed', 'test order date', null, null, null);
-        self::insertData(Patient::getId(5), '2024-11-05', '2024-12-25', 3, 'Pending', 'test ordering of future date. this comment in reality should not be here, but just here to inform its purpose...', null, null, null, null, null);
+        self::insertData(Patient::getId(5), '2024-11-05', '2024-11-06', 3, 'Completed', 'much better on iron level', "take 1mg iron supplement", null, null); // 4
+        self::insertData(Patient::getId(5), '2024-11-05', '2024-12-25', 3, 'Pending', 'test ordering of future date. this comment in reality should not be here, but just here to inform its purpose...', null, null, null, null, null); // 5
 
-        self::insertData(Patient::getId(5), "2024-12-10", "2024-12-11", 25, "Missing", null, null, null, null);
+        // Some missed appointments
+        self::insertData(Patient::getId(5), "2024-12-10", "2024-12-11", Employee::getId(25), "Missing", null, null, null, null); // 6
+        self::insertData(Patient::getId(37), '2024-12-01', '2024-12-05', Employee::getId(26), "Missing", null, null, null, null); // 7
+        self::insertData(Patient::getId(38), '2024-12-03', '2024-12-05', Employee::getId(26), "Missing", null, null, null, null); // 8
+        self::insertData(Patient::getId(39), '2024-12-02', '2024-12-05', Employee::getId(25), "Missing", null, null, null, null); // 9
+        self::insertData(Patient::getId(41), '2024-12-01', '2024-12-02', Employee::getId(3), "Missing", null, null, null, null); // 10
+        self::insertData(Patient::getId(43), '2024-12-02', '2024-12-05', Employee::getId(26), "Missing", null, null, null, null); // 11
+        self::insertData(Patient::getId(43), '2024-12-01', '2024-12-03', Employee::getId(25), "Missing", null, null, null, null); // 12
+        self::insertData(Patient::getId(39), '2024-12-08', '2024-12-09', Employee::getId(3), "Missing", null, null, null, null); // 13
+        self::insertData(Patient::getId(35), '2024-12-03', '2024-12-06', Employee::getId(3), "Missing", null, null, null, null); // 14
+        self::insertData(Patient::getId(37), '2024-12-06', '2024-12-09', Employee::getId(3), "Missing", null, null, null, null); // 15
+        self::insertData(Patient::getId(35), '2024-12-04', '2024-12-05', Employee::getId(26), "Missing", null, null, null, null); // 16
 
-        // // Touchable
-        // Should be inserting prescriptions for each time of day
-        // self::insertData(Patient::getId(37), '2024-12-01', '2024-12-10', 25, "Pending", "Back pain", "apply heat therapy", "Take rest", 'morning');
-        // self::insertData(Patient::getId(38), '2024-12-05', '2024-12-15', 26, "Completed", "High blood pressure", "reduce salt intake",  "Increase exercise", 'afternoon');
-        // self::insertData(Patient::getId(39), '2024-11-28', '2024-12-12', 25, "Pending", "Routine checkup", "general health review",  "Healthy diet", 'night');
-        // self::insertData(Patient::getId(41), '2024-12-02', '2024-12-10', 27, "Completed", "Low vitamin B12", "take B12 supplement",  "Increase sunlight exposure", 'morning');
-        // self::insertData(Patient::getId(43), '2024-11-25', '2024-12-11', 26, "Pending", "Allergy test", "avoid allergens",  null, 'afternoon');
-        // self::insertData(Patient::getId(45), '2024-12-03', '2024-12-12', 25, "Completed", "Mild cold", "drink warm fluids",  "Take over-the-counter medication", null);
-        // self::insertData(Patient::getId(46), '2024-11-20', '2024-12-13', 27, "Pending", "Follow-up on vaccination", "monitor side effects",  "Keep vaccination records updated", 'morning');
-        // self::insertData(Patient::getId(47), '2024-12-01', '2024-12-09', 27, "Completed", "General fatigue", "increase rest",  "Hydrate regularly", 'afternoon');
-        // self::insertData(Patient::getId(48), '2024-12-07', '2024-12-11', 26, "Pending", "Checkup after surgery", "monitor recovery",  "Follow post-surgery guidelines", 'night');
-        // self::insertData(Patient::getId(35), '2024-12-05', '2024-12-09', 25, "Completed", "Minor injury", "apply antiseptic",  "Avoid heavy lifting", null);
+        // Some completed appointments
+        self::insertData(Patient::getId(37), '2024-12-01', '2024-12-10', Employee::getId(26), "Completed", "Back pain", "apply heat therapy", "drink more water", null); // 17
+        self::insertData(Patient::getId(38), '2024-12-05', '2024-12-10', Employee::getId(26), "Completed", "High blood pressure", "reduce salt intake", "Increase exercise", null); // 18
+        self::insertData(Patient::getId(39), '2024-12-01', '2024-12-12', Employee::getId(3), "Completed", "Routine checkup - healthy diet", null, null, null); // 19
+        self::insertData(Patient::getId(40), '2024-12-02', '2024-12-10', Employee::getId(26), "Completed", "Low vitamin B12", "take B12 supplement", "Increase sunlight exposure", null); // 20
+        self::insertData(Patient::getId(41), '2024-12-01', '2024-12-11', Employee::getId(25), "Completed", "Allergy test", null, "take 1 tsp of drug", null); // 21
+        self::insertData(Patient::getId(42), '2024-12-02', '2024-12-12', Employee::getId(3), "Completed", "Mild cold", "drink warm fluids", "Take mucinx 12hr tablet for congestion", null); // 22
+        self::insertData(Patient::getId(43), '2024-12-10', '2024-12-11', Employee::getId(25), "Completed", "Follow-up on vaccination", "monitor side effects", "drink glass of water", null); // 23
+
+        // Patients dont' exist
+        // self::insertData(Patient::getId(47), '2024-12-01', '2024-12-09', Employee::getId(3), "Completed", "General fatigue", "increase rest", "Hydrate regularly", 'afternoon'); // 24
+        // self::insertData(Patient::getId(48), '2024-12-05', '2024-12-11', Employee::getId(25), "Completed", "Checkup after surgery", "monitor recovery", "Follow post-surgery guidelines", 'night'); // 25
+        // self::insertData(Patient::getId(35), '2024-12-05', '2024-12-09', Employee::getId(3), "Completed", "Minor injury", "apply antiseptic", "Avoid heavy lifting", null); // 26
+
+        // Future appointments
+        self::insertData(Patient::getId(5), "2024-12-11", "2024-12-12", Employee::getId(3), "Pending", null, null, null, null); // 27
+        self::insertData(Patient::getId(5), "2024-12-13", "2024-12-13", Employee::getId(3), "Pending", null, null, null, null); // 28
+        self::insertData(Patient::getId(37), '2024-12-25', '2024-12-29', Employee::getId(3), "Pending", null, null, null, null); // 29
+        self::insertData(Patient::getId(38), '2024-12-26', '2024-12-30', Employee::getId(3), "Pending", null, null, null, null); // 30
+        self::insertData(Patient::getId(39), '2024-12-27', '2024-12-31', Employee::getId(3), "Pending", null, null, null, null); // 31
+        self::insertData(Patient::getId(37), '2024-12-15', '2024-12-20', Employee::getId(25), "Pending", null, null, null, null); // 32
+        self::insertData(Patient::getId(38), '2024-12-16', '2024-12-18', Employee::getId(26), "Pending", null, null, null, null); // 33
+        self::insertData(Patient::getId(39), '2024-12-17', '2024-12-19', Employee::getId(25), "Pending", null, null, null, null); // 34 (Updated to Doctor ID 25)
+        self::insertData(Patient::getId(41), '2024-12-18', '2024-12-22', Employee::getId(25), "Pending", null, null, null, null); // 35
+        self::insertData(Patient::getId(43), '2024-12-19', '2024-12-21', Employee::getId(26), "Pending", null, null, null, null); // 36
+        self::insertData(Patient::getId(40), '2024-12-20', '2024-12-23', Employee::getId(3), "Pending", null, null, null, null); // 37
+        self::insertData(Patient::getId(42), '2024-12-21', '2024-12-25', Employee::getId(3), "Pending", null, null, null, null); // 38
+        self::insertData(Patient::getId(43), '2024-12-22', '2024-12-24', Employee::getId(26), "Pending", null, null, null, null); // 39
+        self::insertData(Patient::getId(37), '2024-12-23', '2024-12-27', Employee::getId(3), "Pending", null, null, null, null); // 40
+        self::insertData(Patient::getId(35), '2024-12-24', '2024-12-28', Employee::getId(25), "Pending", null, null, null, null); // 41
 
     }
 
